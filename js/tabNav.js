d8b38aa9a88c63cef75f56ceb5979501d2989ae8
RafaScriptter <nativaeleven@gmail.com>
2026-06-30 14:10:55 -0300

    TabOrcamento javascript interatividade

---
+++ js/tabNav.js
@@ -0,0 +1,33 @@
+export function tabNav() {
+  const dataItemOpcao = document.querySelectorAll("[data-container-opcao]");
+  const dataOpcaoTab = document.querySelectorAll("[data-tab-opcao]");
+
+  const dataOpcaoItemSeguro = document.querySelectorAll(
+    "[data-opcao-item-seguro]",
+  );
+
+  function removeClassAll(element) {
+    element.forEach((item) => item.classList.remove("on"));
+  }
+
+  dataOpcaoItemSeguro.forEach((item) => {
+    item.addEventListener("click", () => {
+      removeClassAll(dataOpcaoItemSeguro);
+      item.classList.add("on");
+    });
+  });
+
+  dataOpcaoTab.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      removeClassAll(dataOpcaoTab);
+      dataOpcaoTab[index].classList.add("on");
+    });
+  });
+
+  dataOpcaoTab.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      removeClassAll(dataItemOpcao);
+      dataItemOpcao[index].classList.add("on");
+    });
+  });
+}
